61cd7b81eac3f15ec210febb8e7ddb8a152cde0a
ValeXL22 <xia.valerio@gmia>
2024-05-16 13:07:10 +0200

    Added random number generator

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        System.out.println(220107);
-        System.out.println("Hello lol");
-        System.out.println(777);
+        Random rand = new Random();
+        int x = rand.nextInt(51);;
+        if (x % 2 == 0) {
+            System.out.println(220107);
+            System.out.println("Hello lol "+x);
+            System.out.println(777);
+        }
     }
 }
+
